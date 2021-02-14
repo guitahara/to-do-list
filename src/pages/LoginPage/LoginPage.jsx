95597e6a1b8759ee220bcebfb16845bb6f55fbfa
Guilherme <guilherme@pop-os.localdomain>
2021-02-13 23:13:36 -0300

    added login layout

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  Label,
+  Input,
+  FormGroup,
+  Button,
+  Row,
+  Col,
+  Jumbotron,
+  Card,
+  Form
+} from 'reactstrap';
 import './login.css';
 
 function LoginPage() {
   return (
     <section>
-      <h1>Login</h1>
+      <Jumbotron fluid>
+        <Container fluid='sm'>
+          <Row>
+            <Col sm={{ size: 6, offset: 3 }} md={{ size: 6, offset: 3 }}>
+              <Card outline color='secondary' className='login-form-card'>
+                <h2 className='title'>Login</h2>
+                <Form>
+                  <FormGroup>
+                    <Label>Username</Label>
+                    <Input type='text' name='username'></Input>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label>Password</Label>
+                    <Input type='password' name='password'></Input>
+                  </FormGroup>
+                  <FormGroup>
+                    <Button color='primary'>Login</Button>
+                    <Link to='/register'>Registrar</Link>
+                  </FormGroup>
+                </Form>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </Jumbotron>
     </section>
   );
 }
